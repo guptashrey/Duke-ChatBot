f4715c2c1d25aa7dbd81fbedf69014ea77452bab
Shrey Gupta <s.gupta@duke.edu>
2023-04-17 00:31:33 +0000

    Added logo

--- scripts/streamlit_app.py
+++ scripts/streamlit_app.py
@@ -3,8 +3,17 @@ from streamlit_chat import message
 import requests
 
 # Setting page title and header
-st.set_page_config(page_title="Duke ChatBot", page_icon=":robot_face:")
-st.markdown("<h1 style='text-align: center;'>Duke ChatBot</h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Duke ChatBot", page_icon=":robot_face:", layout="wide")
+
+
+col_1, col_2 = st.columns([10,90])
+with col_1:
+    st.image("logo.png", use_column_width="auto")
+with col_2:
+    st.title("Duke ChatBot")
+    st.markdown("##### A chatbot that can answer your questions about Pratt School of Engineering!")
+
+st.divider()
 
 # Initialise session state variables
 if 'generated' not in st.session_state:
@@ -27,16 +36,50 @@ def generate_response(prompt):
     # print(st.session_state['messages'])
     return response
 
+
+# # Storing the chat
+# if 'generated' not in st.session_state:
+#     st.session_state['generated'] = []
+
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = []
+
+# def get_text():
+#     input_text = st.text_input("You: ","Hello, how are you?", key="input")
+#     return input_text
+
+
+
+# user_input = get_text()
+
+# if user_input:
+#     output = generate_response(user_input)
+#     # store the output 
+#     st.session_state.past.append(user_input)
+#     st.session_state.generated.append(output)
+
+# if st.session_state['generated']:
+    
+#     for i in range(len(st.session_state['generated'])-1, -1, -1):
+#         message(st.session_state["generated"][i], key=str(i))
+#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
 # container for chat history
-# response_container = st.container()
+#response_container = st.container()
 response_container = st.empty()
 # container for text box
 container = st.container()
 
 with container:
     with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("You:", key='input', height=100)
-        submit_button = st.form_submit_button(label='Send')
+        #user_input = st.text_area("You:", key='input', height=10)
+        col1, col2 = st.columns([80,20])
+        with col1:
+            user_input = st.text_input("Ask me anything...", key='input')
+        with col2:
+            st.write("")
+            st.write("")
+            submit_button = st.form_submit_button(label='Send', use_container_width=True)
 
     if submit_button and user_input:
         output = generate_response(user_input)
@@ -49,8 +92,8 @@ if st.session_state['generated']:
             message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
             message(st.session_state["generated"][i], key=str(i))
 
-# reset everything
-if st.button("Clear Conversation", key="clear"):
+# # reset everything
+if st.button("Clear Chat"):
     response_container.empty()
     st.session_state['generated'] = []
     st.session_state['past'] = []
